16c8ac2d6d78eeb0a02b938418a1c160d5688e57
viktoriia-vishenia <159036621+viktoriia-vishenia@users.noreply.github.com>
2024-02-25 14:28:07 +0300

    initial commit
    initial commit

---
+++ service-client/src/main/java/org/micro/serviceclient/ServiceClientApplication.java
@@ -0,0 +1,15 @@
+package org.micro.serviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceClientApplication.class, args);
+    }
+
+}
